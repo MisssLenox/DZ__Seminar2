ef00a9c21ba3de2718c28606c1bb1e57294981e2
Elena <ivanova123402@mail.ru>
2022-11-16 14:59:28 +0300

    zadaha10

---
+++ Zadaha10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 10.
+//Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа. 
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = ((number / 10) % 10);
+Console.WriteLine (num);
+
